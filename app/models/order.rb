c8a51b001182c8d44e1b28ef3bcc85953b4dcdd6
Raymond <raymond@philippinedev.com>
2021-09-05 16:25:51 +0800

    validation for order

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,3 +1,6 @@
 class Order < ApplicationRecord
   belongs_to :user
+
+  validates :shipping_address, presence: true
+  validates :total, presence: true
 end
--- spec/models/order_spec.rb
+++ spec/models/order_spec.rb
@@ -4,4 +4,9 @@ RSpec.describe Order, type: :model do
   describe 'relationships' do
     it { should belong_to(:user) }
   end
+
+  describe 'validations' do
+    it { should validate_presence_of(:shipping_address) }
+    it { should validate_presence_of(:total) }
+  end
 end
