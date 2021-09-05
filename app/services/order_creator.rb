1f7c1002a25d62f1d10d1fd99501ace44cf54c77
Raymond <raymond@philippinedev.com>
2021-09-06 02:31:59 +0800

    order_creator with test

--- app/services/order_creator.rb
+++ app/services/order_creator.rb
@@ -1,6 +1,36 @@
 class OrderCreator < ActiveInteraction::Base
+  record :user
+  string :shipping_address
+  array :cart_items do
+    hash do
+      record :product
+      integer :quantity
+      integer :price
+    end
+  end
+
   def execute
+    ActiveRecord::Base.transaction do
+      @order = create_order
+      cart_items.each do |item|
+        create_items(item)
+      end
+      @order.save!
+    end
   end
 
   private
+
+  def create_order
+    Order.create!(user_id: user.id, shipping_address: shipping_address, total: 0)
+  rescue ActiveRecord::RecordInvalid => e
+    errors.add(:base, e.message)
+  end
+
+  def create_items(item)
+    @order.order_items.create(product_id: item.id, quantity: item.quantity, price: item.price)
+    @order.total += item.price * item.quantity
+  rescue ActiveRecord::RecordInvalid => e
+    errors.add(:base, e.message)
+  end
 end
--- spec/services/order_creator_spec.rb
+++ spec/services/order_creator_spec.rb
@@ -3,7 +3,32 @@
 require 'rails_helper'
 
 RSpec.describe OrderCreator do
-  it 'sdf' do
-    expect(1).to eq 1
+  let(:user) { create :user }
+  let(:shipping_address) { Faker::Address.full_address }
+  let(:product_one) { create :product }
+  let(:product_two) { create :product }
+  let(:product_three) { create :product }
+  let(:cart_items) do
+    [
+      { product: product_one.id, quantity: 1, price: product_one.price },
+      { product: product_two.id, quantity: 2, price: product_two.price },
+      { product: product_three.id, quantity: 3, price: product_three.price },
+    ]
+  end
+  let(:params) do
+    {
+      user: user.id,
+      shipping_address: shipping_address,
+      cart_items: cart_items
+    }
+  end
+
+  subject { described_class.run!(params) }
+
+  it 'creates an order' do
+    order = subject
+    expect(order.total).to eq cart_items.sum { |x| x.quantity * x.price }
+    expect(order.shipping_address).to eq(shipping_address)
+    expect(order.user_id).to eq(user.id)
   end
 end
