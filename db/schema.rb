c49b64f0849e1dc7ec5821d926dc1750d9f8ed08
Raymond <raymond@philippinedev.com>
2021-09-05 15:47:32 +0800

    schema updates

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_09_05_072335) do
+ActiveRecord::Schema.define(version: 2021_09_05_073823) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "regions", force: :cascade do |t|
+    t.string "title", null: false
+    t.string "country", null: false
+    t.string "currency", null: false
+    t.integer "tax", default: 0
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
