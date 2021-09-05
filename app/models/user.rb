06a7fa6a09c50ca259348b5fdeff718813ee361e
Raymond <raymond@philippinedev.com>
2021-09-05 15:44:22 +0800

    added validation for user

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,4 +2,6 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable,
          :confirmable
+
+  validates :name, presence: true
 end
