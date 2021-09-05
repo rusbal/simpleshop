87d88dcbffc9ee1158727960cec620ff631b9f2f
Raymond <raymond@philippinedev.com>
2021-09-05 16:27:25 +0800

    order_item <-> order relationship test

--- spec/models/order_item_spec.rb
+++ spec/models/order_item_spec.rb
@@ -1,5 +1,13 @@
 require 'rails_helper'
 
 RSpec.describe OrderItem, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe 'relationships' do
+    it { should belong_to(:order) }
+    it { should belong_to(:product) }
+  end
+
+  # describe 'validations' do
+  #   it { should validate_presence_of(:shipping_address) }
+  #   it { should validate_presence_of(:total) }
+  # end
 end
