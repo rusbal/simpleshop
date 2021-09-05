91719b588140452c2c24b110b47dddd508e0db18
Raymond <raymond@philippinedev.com>
2021-09-05 17:19:49 +0800

    user token controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,18 @@
-class ApplicationController < ActionController::Base
+class ApplicationController < ActionController::API
+  include Knock::Authenticable
+  undef_method :current_user
+
+  def success(status = :ok)
+    render_status('success', status)
+  end
+
+  def failure(status = :unprocessable_entity)
+    render_status('failed', status)
+  end
+
+  private
+
+  def render_status(readable_status, status)
+    render json: { status: readable_status }, status: status
+  end
 end
