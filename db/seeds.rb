3bc815e799254eaf432a8b0c0f0f160d0adc3601
Raymond <raymond@philippinedev.com>
2021-09-05 23:57:44 +0800

    seeder for development

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,29 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+def create_users
+  User.destroy_all
+  15.times do
+    user = User.new(email: Faker::Internet.email, name: Faker::Name.name)
+    user.password = 'password'
+    user.password_confirmation = 'password'
+    user.skip_confirmation!
+    user.save!
+  end
+  p "Created #{User.count} users."
+end
+
+def create_regions_and_products
+  Product.destroy_all
+  Region.destroy_all
+
+  50.times do
+    region = FactoryBot.create(:region)
+    rand(1..10).times do
+      FactoryBot.create(:product, region: region)
+    end
+  end
+
+  p "Created #{Region.count} regions."
+  p "Created #{Product.count} products."
+end
+
+create_users
+create_regions_and_products
