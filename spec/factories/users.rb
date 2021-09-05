9b036d32d5155a037ea3db2e3cd56549ff224bd6
Raymond <raymond@philippinedev.com>
2021-09-05 15:33:02 +0800

    factory: users

---
+++ spec/factories/users.rb
@@ -0,0 +1,16 @@
+FactoryBot.define do
+  factory :user do
+    email { Faker::Internet.email }
+    password { 'password ' }
+    password_confirmation { 'password ' }
+    confirmation_token { 'confirmation-token' }
+    confirmed_at { DateTime.current }
+    confirmation_sent_at { DateTime.current }
+    name { Faker::Name.name }
+    admin { false }
+
+    trait :admin do
+      admin { true }
+    end
+  end
+end
