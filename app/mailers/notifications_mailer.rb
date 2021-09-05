dc70d03715f5c3751a50a6ba2f231773bbdc0423
Raymond <raymond@philippinedev.com>
2021-09-05 16:52:25 +0800

    refactor

--- app/mailers/notifications_mailer.rb
+++ app/mailers/notifications_mailer.rb
@@ -1,10 +1,4 @@
 class NotificationsMailer < ApplicationMailer
-
-  # Subject can be set in your I18n file at config/locales/en.yml
-  # with the following lookup:
-  #
-  #   en.notifications_mailer.order_payment_status.subject
-  #
   def order_payment_status(order)
     @order_payment_status = order.payment_status
 
