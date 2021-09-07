ab1a05cd6b6b4d6990849d7cfb8d7a4a8d1e8795
Raymond <raymond@philippinedev.com>
2021-09-07 11:08:36 +0800

    refactor

--- spec/requests/api/v1/users_spec.rb
+++ spec/requests/api/v1/users_spec.rb
@@ -1,6 +1,14 @@
 require 'rails_helper'
 
 RSpec.describe Api::V1::UsersController, type: :request do
+  let!(:user2) do
+    create(:user,
+           id: 1000,
+           admin: false,
+           email: 'another@gmail.com',
+           confirmed_at: confirmed_at,
+           name: 'Pepe')
+  end
   let(:name) { 'Raymond Usbal' }
   let(:confirmed_at) { '2000-01-01 00:00:00'.to_datetime }
   let(:email) { 'raymond@philippinedev.com' }
@@ -20,32 +28,68 @@ RSpec.describe Api::V1::UsersController, type: :request do
     ]
   end
 
-  context 'with customer' do
-    let(:admin) { false }
-    let(:user) { create(:user, email: email, confirmed_at: confirmed_at, name: name) }
-    let!(:user2) { create(:user, email: 'another@gmail.com', confirmed_at: confirmed_at, name: 'Pepe') }
+  describe 'GET #index' do
+    context 'with customer' do
+      let(:admin) { false }
+      let(:user) { create(:user, email: email, confirmed_at: confirmed_at, name: name) }
 
-    describe "GET /api/v1/users" do
-      subject { get '/api/v1/users', headers: headers }
+      describe "GET /api/v1/users" do
+        subject { get '/api/v1/users', headers: headers }
 
-      it 'returns a list of users' do
-        subject
-        expect(response.parsed_body).to eq expected_users
+        it 'returns a list of users' do
+          subject
+          expect(response.parsed_body).to eq expected_users
+        end
+      end
+    end
+
+    context 'with admin user' do
+      let(:admin) { true }
+      let(:user) { create(:user, :admin, email: email, confirmed_at: confirmed_at, name: name) }
+      let(:expected_users) do
+        [
+          {
+            "admin" => false,
+            "confirmed_at" => "2000-01-01T00:00:00.000Z",
+            "email" => "another@gmail.com",
+            "id" => 1000,
+            "name" => "Pepe"
+          },
+          {
+            "admin" => admin,
+            "confirmed_at" => "2000-01-01T00:00:00.000Z",
+            "email" => "raymond@philippinedev.com",
+            "id" => user.id,
+            "name" => "Raymond Usbal"
+          }
+        ]
+      end
+
+      describe "GET /api/v1/users" do
+        subject { get '/api/v1/users', headers: headers }
+
+        it 'returns a list of users' do
+          subject
+          expect(response.parsed_body).to eq expected_users
+        end
       end
     end
   end
 
-  context 'with admin user' do
-    let(:admin) { true }
-    let(:user) { create(:user, :admin, email: email, confirmed_at: confirmed_at, name: name) }
-
-    describe "GET /api/v1/users" do
-      subject { get '/api/v1/users', headers: headers }
-
-      it 'returns a list of users' do
-        subject
-        expect(response.parsed_body).to eq expected_users
-      end
-    end
-  end
+  # describe 'GET #show' do
+  #   context 'with customer' do
+  #     let(:admin) { false }
+  #     let(:user) { create(:user, email: email, confirmed_at: confirmed_at, name: name) }
+  #     let!(:user2) { create(:user, email: 'another@gmail.com', confirmed_at: confirmed_at, name: 'Pepe') }
+  #
+  #     describe "GET /api/v1/users" do
+  #       subject { get '/api/v1/users', headers: headers }
+  #
+  #       it 'returns a list of users' do
+  #         subject
+  #         expect(response.parsed_body).to eq expected_users
+  #       end
+  #     end
+  #   end
+  # end
 end
